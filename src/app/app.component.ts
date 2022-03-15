95baff533e1a2633f914953593d2dae67dd55baa
Gary Glitter Charles <gary@Garys-Mac-mini.local>
2022-03-15 19:48:38 +0530

    heicToAngular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import heic2any from 'heic2any';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'heicUpload';
+  base64Img: any;
+  heicConvert(event: any) {
+    let file = event.target.files[0];
+    console.log(file);
+    heic2any({
+      blob: file,
+      // it can be "image/jpeg", "image/png" or "image/gif"
+      // defaults to "image/png"
+      toType: "image/png"
+    })
+      .then((convertHeicTopng: any) => {
+        var reader = new FileReader();
+        reader.readAsDataURL(convertHeicTopng);
+        reader.onloadend = () => {
+          var base64data = reader.result;
+          this.base64Img = base64data
+          console.log(base64data);
+        }
+      })
+  }
 }
